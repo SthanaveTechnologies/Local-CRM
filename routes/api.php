1b5d94c083df1e3b9833ae9599ddb6cb310755ba
Rahul Kumar Soni <96325448+kumarsoni7999@users.noreply.github.com>
2024-02-15 12:58:20 +0530

    cors enable for locahost:3000

--- routes/api.php
+++ routes/api.php
@@ -15,24 +15,25 @@ use App\Http\Controllers\notesController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::get('/contacts', [crmController::class, 'index']);
-Route::post('/contacts', [crmController::class, 'store']);
-
-Route::get('/colleges', [notesController::class, 'getColleges']);
-Route::post('/colleges', [notesController::class, 'storeCollege']);
-
-Route::get('/courses', [notesController::class, 'getCourses']);
-Route::post('/courses', [notesController::class, 'storeCourse']);
-
-Route::get('/subject', [notesController::class, 'getSubjectsByCourseId']);
-Route::post('/subject', [notesController::class, 'storeSubject']);
-
-Route::get('/units', [notesController::class, 'getUnitsBySubjectId']);
-Route::post('/units', [notesController::class, 'storeUnit']);
-
-Route::get('/topics', [notesController::class, 'getTopicsByUnitId']);
-Route::post('/topics', [notesController::class, 'storeTopics']);
-
-Route::get('/references', [notesController::class, 'getReferencesByTopicId']);
-Route::post('/references', [notesController::class, 'storeReferences']);
+Route::middleware(['cors'])->group(function () {
+    Route::get('/contacts', [crmController::class, 'index']);
+    Route::post('/contacts', [crmController::class, 'store']);
+    
+    Route::get('/colleges', [notesController::class, 'getColleges']);
+    Route::post('/colleges', [notesController::class, 'storeCollege']);
+    
+    Route::get('/courses', [notesController::class, 'getCourses']);
+    Route::post('/courses', [notesController::class, 'storeCourse']);
+    
+    Route::get('/subject', [notesController::class, 'getSubjectsByCourseId']);
+    Route::post('/subject', [notesController::class, 'storeSubject']);
+    
+    Route::get('/units', [notesController::class, 'getUnitsBySubjectId']);
+    Route::post('/units', [notesController::class, 'storeUnit']);
+    
+    Route::get('/topics', [notesController::class, 'getTopicsByUnitId']);
+    Route::post('/topics', [notesController::class, 'storeTopics']);
+    
+    Route::get('/references', [notesController::class, 'getReferencesByTopicId']);
+    Route::post('/references', [notesController::class, 'storeReferences']);
+});
